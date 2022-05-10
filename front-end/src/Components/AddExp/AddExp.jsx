86b0dfb6f8e8028760c71b210ff05556b5530c54
Hajar <hajar.dami.14@gmail.com>
2022-05-10 22:44:15 +0100

    AddExpForm

---
+++ front-end/src/Components/AddExp/AddExp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from '../Home/header/Header';
+import AddExpForm from "./AddExpForm"
+function AddExp()
+ { return(
+ <div><Header/> <AddExpForm/></div>)
+
+}
+  export default AddExp;
