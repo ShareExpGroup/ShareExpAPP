4d12b75d7ea72b8ff6f3cf21a40bf1ed91331cbb
Hajar <hajar.dami.14@gmail.com>
2022-05-22 17:50:44 +0100

    commentsBack

--- back-end/src/main/java/com/shareExp/backend/model/ShareExpClient.java
+++ back-end/src/main/java/com/shareExp/backend/model/ShareExpClient.java
@@ -5,11 +5,14 @@ import com.shareExp.backend.enumeration.Role;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class ShareExpClient extends User{
-
-
+/*
+    @OneToMany
+    @JoinColumn(name = "id_share")
+      private Set<Comment> items;*/
 
 
     public ShareExpClient(String first_name, String last_name, Date birth_date,
