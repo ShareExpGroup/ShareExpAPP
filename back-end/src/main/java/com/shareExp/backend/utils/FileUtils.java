f24222e50c6a23e8afa217cd753abe273b7e0c78
Amina Chaabane <amina.chaabane34@gmail.com>
2022-05-24 21:01:02 +0100

    solve some issues with addExperience

--- back-end/src/main/java/com/shareExp/backend/utils/FileUtils.java
+++ back-end/src/main/java/com/shareExp/backend/utils/FileUtils.java
@@ -1,5 +1,6 @@
 package com.shareExp.backend.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.multipart.MultipartFile;
 
